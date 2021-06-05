1aa88b2251855a8c8b7fbcaa430cedf9fb2d0821
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-05 07:14:19 +0000

    [Add] followings・followersメソッド

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -10,4 +10,14 @@ class RelationshipsController < ApplicationController
     redirect_to request.referer
   end
   
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+  
+  def followers
+    user = User.find(params{:user_id})
+    @users = user.followers
+  end
+  
 end
