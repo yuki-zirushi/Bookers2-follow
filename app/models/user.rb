9275674968f6fe9bc00eff6a695e3ed1af76f90f
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-05 06:59:16 +0000

    [Add] インスタンスメソッド User

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,21 @@ class User < ApplicationRecord
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
   
-attachment :profile_image, destroy: false
+  attachment :profile_image, destroy: false
 
-validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
-validates :introduction, length: { maximum: 50 }
+  validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
 
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
+  end
+  
 end
