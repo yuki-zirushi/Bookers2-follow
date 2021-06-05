e9547f511c03cc882a9f5bc6765ed2139c7ff6f1
yuki-zirushi <yuko.12.151@gmail.com>
2021-06-05 06:33:34 +0000

    [Create] Relationshipモデル・テーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_062142) do
+ActiveRecord::Schema.define(version: 2021_06_05_063204) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
